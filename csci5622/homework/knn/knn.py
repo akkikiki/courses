53232f8c6c17bcf29080dd193301774fbd23b610
Yoshinari Fujinuma <fujinumay@gmail.com>
2016-08-23 16:30:20 -0600

    Now the tests are passed

--- csci5622/homework/knn/knn.py
+++ csci5622/homework/knn/knn.py
@@ -68,37 +68,22 @@ class Knearest:
         Now the values are either 0 or 2
         """
         nearest_labels = [self._y[x] for x in item_indices]
-        # counts = numpy.bincount(map(lambda x: x + 1, nearest_labels))
-        #print self._y
-        #print counts
 
         labels_dic = defaultdict(int)
         for i in nearest_labels:
             labels_dic[i] += 1
 
-        # TODO: You should not assume that the classes are only two!
-        # TODO: constructing a dictionary type here
-        # if len(counts) == 3 and counts[0] == counts[2]:
-        #     # handling cases that only labels -1 exist as k-nearest neighbors
-        #     return numpy.median(counts) - 1
-        # else:
-        #     return numpy.argmax(counts) - 1
+        # You should not assume that the target classes are only two!
+        top_occuring_label, top_occuring_label_val = sorted(labels_dic.items(), key= lambda x:x[1], reverse=True)[0]
 
-        num_labels = labels_dic.values()
-        # TODO: Find duplicates or ties!
-        num_labels_set = set(num_labels)
-        # TODO: Does this part really finiding ties?
-        if len(num_labels) != len(num_labels_set):
-            return numpy.median(nearest_labels)
-        else:
-            # TODO: This part should be finding ties!
-            # sort by the values, not by the keys
+        # This part should be finding ties!
+        if len(labels_dic) > 1:
+            second_occuring_label, second_occuring_label_val = sorted(labels_dic.items(), key= lambda x:x[1], reverse=True)[1]
 
-            a = sorted(labels_dic.items(), key= lambda x:x[1], reverse=True)[0][1]
+            if top_occuring_label_val == second_occuring_label_val:
+                return numpy.median(nearest_labels)
 
-            return a
-
-        #return self._y[item_indices[0]]
+        return top_occuring_label
 
     def classify(self, example):
         """

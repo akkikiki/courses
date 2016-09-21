bbfcd29beecd00717e883dd18a003d725654083c
Yoshinari Fujinuma <fujinumay@gmail.com>
2016-09-21 13:55:58 -0600

    Adding train, test split

--- csci5622/homework/svm/sklearn/svm_report.py
+++ csci5622/homework/svm/sklearn/svm_report.py
@@ -1,10 +1,14 @@
+import numpy as np
 from sklearn import svm
+from sklearn.cross_validation import train_test_split
 
 # Import the MNIST dataset
 # http://scikit-learn.org/stable/modules/svm.html
 
 """
 1.  Use the Sklearn implementation of support vector machines to train a classifier to distinguish 3's from 8's (using the MNIST data from the KNN homework).
+1.  Try at least five values of the regularization parameter _C_ and at least two kernels.  Comment on performance for the varying parameters you chose by either testing on a hold-out set or performing cross-validation. 
+1.  Give examples of support vectors from each class when using a linear kernel.
 """
 
 class Numbers:
@@ -27,13 +31,32 @@ class Numbers:
         f.close()
 
 data = Numbers("../data/mnist.pkl.gz")
+X = []
+y = []
+hold_out_data = 100
 
 
-X = [[0, 0], [1, 1]]
-y = [0, 1]
+# Filtering out labels other than 3 or 8
+for i in range(len(data.train_y)):
+    if data.train_y[i] == 3:
+        y.append(-1)
+        X.append(data.train_x[i])
+    elif data.train_y[i] == 8:
+        y.append(1)
+        X.append(data.train_x[i])
+ 
+
+C = [0.01, 0.1, 1.0, 10.0, 100.0]
+random_state = np.random.RandomState(0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2, random_state=random_state)
+
+#X = [[0, 0], [1, 1]]
+#y = [0, 1]
 clf = svm.SVC()
-clf.fit(X, y)
-print("Prediction Result: %s" % clf.predict([[2., 2.]]))
+#clf.fit(X[:-hold_out_data], y[:-hold_out_data])
+clf.fit(X_train, y_test)
+
+print("Prediction Result: %s" % clf.predict([X[hold_out_data]]))
 
 # get support vectors
 print(clf.support_vectors_)

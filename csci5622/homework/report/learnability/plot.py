ca250ed3a39021a61ddc4724f87382bfe3ab3c5d
Yoshinari Fujinuma <fujinumay@gmail.com>
2016-10-12 19:35:23 -0600

    Finished problem 2

--- csci5622/homework/report/learnability/plot.py
+++ csci5622/homework/report/learnability/plot.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 #if ylim is not None:
 #    plt.ylim(*ylim)
 a = np.arange(20)
-plt.plot([1,2], [1,4], 'ro')
-plt.plot([4], [3], 'bo')
+plt.plot([1,2], [1,4], 'ro', ms=12)
+plt.plot([4], [3], 'bo', ms=12)
 #plt.plot(a, 3.0/2*a)
 plt.plot(a, 4.0/5*a)
 plt.axis([0, 5, 0, 5])
